9ecfaeb6f129c5efe0e0d3b774e3123fa412b5b5
Robin Monnier <monnier.robin@gmail.com>
2018-04-12 16:26:21 +0200

    add gittalk

--- js/gittalk.js
+++ js/gittalk.js
@@ -1,9 +1,9 @@
 const gitalk = new Gitalk({
     clientID: '8f7108daaf3114f623ff',
     clientSecret: '0d5c25003767ab1401990a3caabaf38e70f11d6e',
-    repo: 'pm2-docsify-proto',
-    owner: 'rmonnier',
-    admin: ['rmonnier'],
+    repo: 'pm2-doc',
+    owner: 'keymetrics',
+    admin: ['keymetrics'],
     id: md5(window.location.pathname),
     // facebook-like distraction free mode
     distractionFreeMode: false
@@ -15,7 +15,7 @@ wrapper.appendChild(document.createElement('hr'));
 var container = document.createElement('div');
 container.id = 'gittalk';
 wrapper.appendChild(container);
-document.querySelector('section.content').appendChild(wrapper)
+document.querySelector('section.content').insertBefore(wrapper, document.querySelector('footer.markdown-section.copyright'));
 $(document).ready(function() {
   gitalk.render('gittalk');
 });
