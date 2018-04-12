12525774876cd1eea401c055cac99108df1844da
Robin Monnier <monnier.robin@gmail.com>
2018-04-12 17:50:40 +0200

    gather all js into one file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,143 @@
+const gitalk = new Gitalk({
+  clientID: '918c405abac62eacf227',
+  clientSecret: '7c102fd9965dbe94ce2fb6006ccadcac8f2b9d44',
+  repo: 'doc-pm2',
+  owner: 'keymetrics',
+  admin: ['keymetrics'],
+  id: md5(window.location.pathname),
+  // facebook-like distraction free mode
+  distractionFreeMode: false
+})
+
+var wrapper = document.createElement('div');
+wrapper.className = "markdown-section";
+wrapper.appendChild(document.createElement('hr'));
+var container = document.createElement('div');
+container.id = 'gittalk';
+wrapper.appendChild(container);
+document.querySelector('section.content').insertBefore(wrapper, document.querySelector('footer.markdown-section.copyright'));
+$(document).ready(function() {
+gitalk.render('gittalk');
+});
+
+
+
+$(function() {
+  logoClick();
+  pWarn();
+  trasformH2();
+  smoothScroll (500);
+  anchorH2();
+});
+
+function helper(className, content) {
+  return ("<p class=\"" + className + "\">" + (content.slice(5).trim()) + "</p>");
+}
+
+function transformClass(text) {
+  var result;
+  if (/^!&gt;/.test(text)) {
+      result = helper('tip', text);
+  } else if (/^\?&gt;/.test(text)) {
+      result = helper('warn', text);
+  } else {
+      result = "<p>" + text + "</p>";
+  }
+  return result;
+}
+
+// if markdown & rendered HTML contains contains 'p tag &gt'
+// render as p class=warn
+function pWarn() {
+  var allPtag = document.getElementsByTagName("p");
+  var length = allPtag.length;
+  var text = [];
+
+
+
+  for (var i = 0; i < length; i++) {
+      if(allPtag[i].innerHTML.indexOf("&gt") !== -1) {
+      text[i] = transformClass(allPtag[i].innerHTML);
+      allPtag[i].outerHTML = text[i];
+      }
+  }
+}
+
+function logoClick() {
+  $('.PM2_logo').click(function() {
+      $(this).toggleClass('close');
+  });
+}
+
+function smoothScroll (duration) {
+$('#main a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        // event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, duration);
+    }
+});
+}
+
+function trasformH2() {
+var heading = document.querySelectorAll('h2');
+var length = heading.length;
+var output = {};
+
+for (var i = length - 1; i >= 0; i--) {
+  var slug = slugify(heading[i].innerText);
+  var text = heading[i].innerText;
+  output[i] = "<h2 id=\"" + slug + "\"><a href=\"#" + slug + "\" data-id=\"" + slug + "\" class=\"anchor\"><span>" + text + "</span></a></h2" + ">";
+  heading[i].outerHTML = output[i];
+}
+}
+
+// from docsify.js
+var hasOwn = Object.prototype.hasOwnProperty;
+var cache$1 = {};
+var re = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~]/g;
+
+function lower(string) {
+return string.toLowerCase();
+}
+
+function slugify(str) {
+if (typeof str !== 'string') {
+  return '';
+}
+
+var slug = str
+  .trim()
+  .replace(/[A-Z]+/g, lower)
+  .replace(/<[^>\d]+>/g, '')
+  .replace(re, '')
+  .replace(/\s/g, '-')
+  .replace(/-+/g, '-')
+  .replace(/^(\d)/, '_$1');
+var count = cache$1[slug];
+
+count = hasOwn.call(cache$1, slug) ? count + 1 : 0;
+cache$1[slug] = count;
+
+if (count) {
+  slug = slug + '-' + count;
+}
+
+return slug;
+}
+
+function anchorH2() {
+  var urlHash = window.location.href.split("#")[1];
+  if (urlHash &&  $('#' + urlHash).length )
+        $('html,body').animate({
+            scrollTop: $('#' + urlHash).offset().top
+        }, 1000);
+}
+
 (function() {
   // ok
     function setSidebarBreakpoints() {
@@ -51,7 +191,7 @@
     function configureSearchBarDocSearch() {
         docsearch({
             appId: 'VOHJUDHWT7',
-            apiKey: '287f3974c9d415757200ffa7052bca0f',
+            apiKey: '65aa0e3b2df3e2c47669de31f26cc562',
             indexName: 'pm2',
             inputSelector: '.search_input',
             debug: true // Set debug to true if you want to inspect the dropdown
@@ -198,7 +338,7 @@
     }
 
     $(document).ready(function(event) {
-        configureSearchBar();
+        // configureSearchBar();
         logoTogglesSidebarOnMobile();
         setSidebarBreakpoints();
         createSidebarLinkClickHandler();
