2c425060b10802a1ff60ac4927d9c4d1a875da43
Robin Monnier <monnier.robin@gmail.com>
2018-04-13 17:13:16 +0200

    set gitalk in chinese

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-const gitalk = new Gitalk({
+var gitalk = new Gitalk({
   clientID: '918c405abac62eacf227',
   clientSecret: '7c102fd9965dbe94ce2fb6006ccadcac8f2b9d44',
   repo: 'doc-pm2',
   owner: 'keymetrics',
   admin: ['robin-monnier', 'vmarchaud', 'Unitech'],
-  language: 'en',
+  language: document.documentElement.lang === 'ch' ? 'zh-CN' : 'en',
   id: md5(window.location.pathname),
   // facebook-like distraction free mode
   distractionFreeMode: false
